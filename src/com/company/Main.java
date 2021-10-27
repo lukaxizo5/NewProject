a8dd986a3448328f3c4ec721b4e1d6e3af5b07e0
Luka Khizambareli <lukakhizambareli@gmail.com>
2021-10-27 18:50:17 +0400

    first

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            String s = sc.nextLine();
+            //create lol daka:varchar lpo:int
+            //get lol
+            //add lol pkk 21
+            if (s.charAt(0)=='c'){
+                
+            }
+        }
+    }
+}
